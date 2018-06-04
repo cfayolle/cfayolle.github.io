3ae3e1016510f03f84a75cfc55e8d0271355c6f5
Cody Fayolle <cfayolle@umich.edu>
2018-06-04 15:54:24 -0500

    background

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {} from 'react-router-dom'
 import {Navbar, NavItem} from 'react-materialize'
 
 const Nav = () => {
   return (
-    <Navbar right className='grey'>
+    <Navbar right className='nav'>
       <NavItem className='navBtn' to='/home' >About Me</NavItem>
       <NavItem className='navBtn' to='/home' >Projects</NavItem>
       <NavItem className='navBtn' to='/home' >Contact Me</NavItem>
