2e5b1778e63db37ffc26c6e8705dc2f6029d73a3
Cody Fayolle <cfayolle@umich.edu>
2018-06-05 16:24:07 -0500

    updated about section

--- src/components/about.js
+++ src/components/about.js
@@ -3,11 +3,20 @@ import React from 'react'
 const About = () => {
   return (
     <div className="about">
-      <div>
-        <img src="./myFace.jpg" alt="Cody's Headshot"/>
+      <div className="row">
+        <div className="col s4">
+          <img className="responsive-img" src="./myFace.jpg" alt="Cody's Headshot"/>
+        </div>
+        <div className="col s8">
+          <h3>About Me</h3>
+          <p>After completing college, I spent a year in Nashville working as a research assistant and composing/producing music during my free time. As my interest in medicine began to decline, I stumbled upon programming and quickly fell in love with the intricacies of creatively solving technical problems.</p>
+          <div>
+
+          </div>
+        </div>
       </div>
       <div>
-        <p>Text</p>
+        <h1>Skills</h1>
       </div>
     </div>
   )
--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="home">
-      <h1 className="titleName"> Cody Fayolle </h1>
-      <h3>Fullstack Engineer</h3>
+      <div className="home-text">
+        <h1 className="titleName"> Cody Fayolle </h1>
+        <h3>Fullstack Engineer</h3>
+      </div>
+      <img className="responsive-img homeBack" src="./back.jpg" alt="Home Background"/>
     </div>
   )
 }
