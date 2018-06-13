67a874717dc18f8641043fbe9e27db72e9273e1f
Cody Fayolle <cfayolle@umich.edu>
2018-06-13 09:13:45 -0500

    adjust nav bar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import {} from 'react-router-dom'
-import {Navbar, NavItem} from 'react-materialize'
 
 const Nav = () => {
   return (
-    <Navbar right className='nav'>
-      <NavItem className='navBtn' href='/#about' >About Me</NavItem>
-      <NavItem className='navBtn' href='/#projects' >Projects</NavItem>
-      <NavItem className='navBtn' href='/#contact' >Contact</NavItem>
-    </Navbar>
+    <div className='nav'>
+      <a className='navBtn' href='/#about' >About Me</a>
+      <a className='navBtn' href='/#projects' >Projects</a>
+      <a className='navBtn' href='/#contact' >Contact</a>
+    </div>
   )
 }
 
